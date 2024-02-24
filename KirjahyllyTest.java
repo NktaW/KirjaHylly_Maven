bdeeb35fd86c367269003dcccbee9c1684d9106b
NktaW <nikita.waara@hotmail.com>
2024-02-24 11:55:59 +0200

    lisätään kommentit

--- KirjahyllyTest.java
+++ KirjahyllyTest.java
@@ -5,18 +5,22 @@ class KirjahyllyTest {
     Kirjahylly kirjahylly = new Kirjahylly();
 
     @Test
+    //lisaaKirja-testissä lisätään kirja ja tarkistetaan, että kirjojen määrä on kasvanut.
     void lisaaKirja() {
         kirjahylly.lisaaKirja("Testikirja");
         assertEquals(1, kirjahylly.getKirjojaHyllyssa());
     }
 
     @Test
+    //viimeksiLisatty-testissä lisätään kirja ja tarkistetaan, että viimeksi lisätty kirja on oikea.
     void viimeksiLisatty() {
         kirjahylly.lisaaKirja("Testikirja");
         assertEquals("Testikirja", kirjahylly.viimeksiLisatty());
     }
 
     @Test
+    //onkoKirjaHyllyssa-testissä lisätään kirja ja tarkistetaan, että kirja löytyy hyllystä.
+        // Lisäksi tarkistetaan, että kirja, jota ei ole lisätty, ei löydy hyllystä.
     void onkoKirjaHyllyssa() {
         kirjahylly.lisaaKirja("Testikirja");
         assertTrue(kirjahylly.onkoKirjaHyllyssa("Testikirja"));
@@ -24,6 +28,8 @@ class KirjahyllyTest {
     }
 
     @Test
+    //getKirjojaHyllyssa-testissä tarkistetaan, että aluksi kirjoja ei ole hyllyssä.
+        // Lisätään kirja ja tarkistetaan, että kirjojen määrä on kasvanut.
     void getKirjojaHyllyssa() {
         assertEquals(0, kirjahylly.getKirjojaHyllyssa());
         kirjahylly.lisaaKirja("Testikirja");
